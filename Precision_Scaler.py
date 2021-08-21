4378fa5ba71fcb4e5c1799fb9b48c58919b6d3dc
APEC <87300864+EugeneDudavkin@users.noreply.github.com>
2021-08-21 22:49:09 +0300

    Update Precision_Scaler.py

--- Precision_Scaler.py
+++ Precision_Scaler.py
@@ -20,7 +20,7 @@
 bl_info = {
     "name": "Precision Scaler",
     "author": "APEC",
-    "version": (0, 0, 1),
+    "version": (0, 1, 1),
     "blender": (2, 93, 0),
     "location": "View3d > N-Panel > PreScaler",
     "description": "Scale selected by defined length", 
@@ -50,26 +50,39 @@ def set_float2(self, value):
     scale_props.default_second = value
 
 def distance_between():
-    obj = bpy.context.edit_object
-    me = obj.data
-    bm = bmesh.from_edit_mesh(me)
+    active_object = bpy.context.active_object
+    selected_objects = bpy.context.selected_objects
+    
+    if active_object and active_object.type == 'MESH' and bpy.context.mode == 'EDIT_MESH': 
+        obj = bpy.context.edit_object
+        me = obj.data
+        bm = bmesh.from_edit_mesh(me)
 
-    elem_list1 = []
+        elem_list = []
 
-    for g in bm.select_history:
-        elem_list1.append(g)
+        for g in bm.select_history:
+            elem_list.append(g)
             
-    point_first  = elem_list1[0].co
-    point_second = elem_list1[1].co
+        if len(elem_list) == 2:
+            point_first  = elem_list[0].co
+            point_second = elem_list[1].co
 
-    distance = (point_second - point_first).length    
+            distance = (point_second - point_first).length
+        
+    if len(selected_objects) == 2:
+        if active_object and active_object.type == 'MESH' and bpy.context.mode == 'OBJECT':
+            obj1 = selected_objects[0].matrix_world.to_translation()
+            obj2 = selected_objects[1].matrix_world.to_translation()
+
+            distance = (obj1 - obj2).length
+        
     return distance
 
 
 class PRESCALE_OT_operator(Operator):
     bl_idname = "precision.scaler"
     bl_label = "Precision Scaler"
-    #bl_description = "Scale by defined length"
+    bl_description = "Scale = Set / Get"
 
     action: bpy.props.EnumProperty (
         items=[('GET','GET',''),
@@ -77,73 +90,139 @@ class PRESCALE_OT_operator(Operator):
                ('SCALE','SCALE','')])
                
     def execute(self, context):
-        scale_props = context.scene.PRESCALE_PG_props       
-    
-        if self.action == "GET":            
+        scale_props = context.scene.PRESCALE_PG_props        
+        active_object = context.active_object
+        current_mode = bpy.context.object.mode
+                
+        if self.action == "GET":
             try:
                 scale_props.l1 = distance_between() 
             except:
-                self.report({'WARNING'}, 'Need to click on first and second vertices')
-                return {'CANCELLED'}           
-        
+                if bpy.context.mode == 'EDIT_MESH':
+                    self.report({'WARNING'}, 'Need to click on first and second vertices')                    
+                if bpy.context.mode == 'OBJECT':
+                    self.report({'WARNING'}, 'Need to select two objects')
+                return {'CANCELLED'}
+                
         if self.action == "SET":
             try:
                 scale_props.l2 = distance_between()
             except:
-                self.report({'WARNING'}, 'Need to click on first and second vertices')
+                if bpy.context.mode == 'EDIT_MESH':
+                    self.report({'WARNING'}, 'Need to click on first and second vertices')
+                if bpy.context.mode == 'OBJECT':
+                    self.report({'WARNING'}, 'Need to select two objects')
                 return {'CANCELLED'}
+
+#            if self.action == "SCALE":                    
+#                real_cursor = bpy.context.scene.cursor.matrix
+#                real_transform = bpy.context.scene.transform_orientation_slots[0].type
+#                real_pivot = bpy.context.scene.tool_settings.transform_pivot_point 
+            
+#                bpy.context.scene.cursor.matrix = bpy.context.object.matrix_world
+#                obj_origin = bpy.context.scene.cursor.matrix
+            
+#                if scale_props.selection_only == True:  
+#                    bpy.ops.object.mode_set(mode='EDIT') 
+                
+#                if scale_props.selection_only == False:  
+#                    bpy.ops.object.mode_set(mode='OBJECT')            
+            
+#                if scale_props.use_cursor == True:
+#                    bpy.context.scene.cursor.matrix = real_cursor
+#                    bpy.context.scene.transform_orientation_slots[0].type = 'CURSOR'
+#                    bpy.context.scene.tool_settings.transform_pivot_point = 'CURSOR'
+
+#                    if scale_props.selection_only == False: 
+#                        bpy.context.scene.tool_settings.use_transform_data_origin = True
+#                        bpy.ops.object.origin_set(type='ORIGIN_CURSOR', center='MEDIAN')                
+#                        bpy.ops.transform.transform(mode='ALIGN', orient_type='CURSOR')
+#                        bpy.context.scene.tool_settings.use_transform_data_origin = False           
+            
+#                scale = scale_props.l2 / scale_props.l1      
+            
+#                if scale_props.use_x == True:
+#                    x = scale
+            
+#                if scale_props.use_x == False:
+#                    x = 1
+            
+#                if scale_props.use_y == True:
+#                    y = scale
+            
+#                if scale_props.use_y == False:
+#                    y = 1
+            
+#                if scale_props.use_z == True:
+#                    z = scale
+            
+#                if scale_props.use_z == False:
+#                    z = 1
+                
+#                bpy.ops.transform.resize(value=(x, y, z))                
+            
+#                bpy.context.scene.transform_orientation_slots[0].type = real_transform
+#                bpy.context.scene.tool_settings.transform_pivot_point = real_pivot
+            
+#                if scale_props.selection_only == False:
+#                    bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)            
+#                    bpy.context.scene.cursor.matrix = obj_origin
+#                    bpy.context.scene.tool_settings.use_transform_data_origin = True
+#                    bpy.ops.object.origin_set(type='ORIGIN_CURSOR', center='MEDIAN')            
+#                    bpy.ops.transform.transform(mode='ALIGN', orient_type='CURSOR')
+#                    bpy.context.scene.tool_settings.use_transform_data_origin = False
+            
+#                bpy.context.scene.cursor.matrix = real_cursor
+
+#                bpy.ops.object.mode_set(mode='EDIT')
         
         if self.action == "SCALE":                    
-            real_cursor = bpy.context.scene.cursor.matrix
-            real_transform = bpy.context.scene.transform_orientation_slots[0].type 
+            real_transform = bpy.context.scene.transform_orientation_slots[0].type
+            real_pivot = bpy.context.scene.tool_settings.transform_pivot_point
             
-            bpy.context.scene.cursor.matrix = bpy.context.object.matrix_world
-            obj_origin = bpy.context.scene.cursor.matrix
-              
-            bpy.ops.object.mode_set(mode='OBJECT')            
+            if scale_props.selection_only == True:
+                bpy.ops.object.mode_set(mode='EDIT') 
+                
+            if scale_props.selection_only == False:
+                bpy.ops.object.mode_set(mode='OBJECT')
             
             if scale_props.use_cursor == True:
-                bpy.context.scene.cursor.matrix = real_cursor
-                bpy.context.scene.transform_orientation_slots[0].type = 'CURSOR' 
-                bpy.context.scene.tool_settings.use_transform_data_origin = True
-                bpy.ops.object.origin_set(type='ORIGIN_CURSOR', center='MEDIAN')                
-                bpy.ops.transform.transform(mode='ALIGN', orient_type='CURSOR')
-                bpy.context.scene.tool_settings.use_transform_data_origin = False               
-            
-            scale = scale_props.l2 / scale_props.l1        
+                bpy.context.scene.transform_orientation_slots[0].type = 'CURSOR'
+                bpy.context.scene.tool_settings.transform_pivot_point = 'CURSOR'
+                            
+            scale = scale_props.l2 / scale_props.l1      
             
             if scale_props.use_x == True:
-                bpy.context.object.scale[0] = scale
+                x = scale
             
             if scale_props.use_x == False:
-                bpy.context.object.scale[0] = 1
+                x = 1
             
             if scale_props.use_y == True:
-                bpy.context.object.scale[1] = scale
+                y = scale
             
             if scale_props.use_y == False:
-                bpy.context.object.scale[1] = 1
+                y = 1
             
             if scale_props.use_z == True:
-                bpy.context.object.scale[2] = scale
+                z = scale
             
             if scale_props.use_z == False:
-                bpy.context.object.scale[2] = 1
+                z = 1
                 
-            bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)
-            bpy.context.scene.transform_orientation_slots[0].type = real_transform
-            bpy.context.scene.cursor.matrix = obj_origin
-            bpy.context.scene.tool_settings.use_transform_data_origin = True
-            bpy.ops.object.origin_set(type='ORIGIN_CURSOR', center='MEDIAN')            
-            bpy.ops.transform.transform(mode='ALIGN', orient_type='CURSOR')
-            bpy.context.scene.tool_settings.use_transform_data_origin = False
+            bpy.ops.transform.resize(value=(x, y, z))                
             
-            bpy.context.scene.cursor.matrix = real_cursor
+            bpy.context.scene.transform_orientation_slots[0].type = real_transform
+            bpy.context.scene.tool_settings.transform_pivot_point = real_pivot
+            
+            if scale_props.selection_only == False:
+                bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)  
 
-            bpy.ops.object.mode_set(mode='EDIT')
+            #bpy.ops.object.mode_set(mode='EDIT')
+            bpy.ops.object.mode_set(mode=current_mode)
 
-        #print("----------------------")
-        #print("Scale =", scale)
+#        print("----------------------")
+#        print("Scale =", scale)
         return {'FINISHED'}
 
 
@@ -176,22 +255,32 @@ class PRESCALE_PG_props(PropertyGroup):
     use_x: bpy.props.BoolProperty (
         name=" X", 
         default = True, 
-        description="Use local X axis to scale")
+        description="Use local X axis to scale"
+    )
         
     use_y: bpy.props.BoolProperty (
         name=" Y", 
         default = True, 
-        description="Use local Y axis to scale")
+        description="Use local Y axis to scale"
+    )
         
     use_z: bpy.props.BoolProperty (
         name=" Z", 
         default = True, 
-        description="Use local Z axis to scale")
+        description="Use local Z axis to scale"
+    )
         
     use_cursor: bpy.props.BoolProperty (
         name="Cursor as origin", 
         default = False, 
-        description="Use 3D Cursor as origin to scale")
+        description="Use 3D Cursor as origin to scale"
+    )
+        
+    selection_only: bpy.props.BoolProperty (
+        name="Selection only", 
+        default = False, 
+        description="Use only selected elements to scale"
+    )
         
     
 class PRESCALE_PT_panel(Panel):
@@ -199,14 +288,11 @@ class PRESCALE_PT_panel(Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = 'PreScale'
-    
-    @classmethod
-    def poll(cls, context):
-        ob = context.active_object
-        return(ob and ob.type == 'MESH' and context.mode == 'EDIT_MESH')
-    
-    def draw(self, context):               
+   
+    def draw(self, context):                       
         scale_props = context.scene.PRESCALE_PG_props
+        active_object = context.active_object
+        
         layout = self.layout
         col = layout.column(align = True)
         row1 = col.row(align = True) 
@@ -227,11 +313,22 @@ class PRESCALE_PT_panel(Panel):
         row3.prop(scale_props, "use_z")        
         
         col = layout.column(align = True)
-        row4 = col.row(align = True)        
-        row4.prop(scale_props, "use_cursor") 
-        row4.scale_x = 0.5  
-        row4.scale_y = 1.3            
-        row4.operator("precision.scaler", icon="TRANSFORM_ORIGINS", text="Scale").action = "SCALE"
+        sub_row = col.row()
+        sub_col1 = sub_row.column()
+        sub_sub_row = sub_col1.row() 
+        sub_sub_col = sub_sub_row.column()             
+#        sub_sub_col.prop(scale_props, "use_cursor", toggle=True)
+        sub_sub_col.prop(scale_props, "use_cursor")
+        sub_sub_row = sub_col1.row() 
+        sub_sub_col = sub_sub_row.column()
+        if active_object and active_object.type == 'MESH' and context.mode == 'OBJECT':
+            sub_sub_col.enabled = False  
+        sub_sub_col.prop(scale_props, "selection_only") 
+        
+        sub_col2 = sub_row.column()
+        sub_col2.scale_x = 0.725  
+        sub_col2.scale_y = 2            
+        sub_col2.operator("precision.scaler", icon="TRANSFORM_ORIGINS", text="Scale").action = "SCALE"
 
 
 # Add-ons Preferences Update Panel
@@ -256,7 +353,7 @@ def update_panel(self, context):
         print("\n[{}]\n{}\n\nError:\n{}".format(__name__, message, e))
         pass
 
-    
+
 class PreScalerPreferences(AddonPreferences):
     # this must match the addon name, use '__package__'
     # when defining this in a submodule of a python package.
@@ -272,6 +369,10 @@ class PreScalerPreferences(AddonPreferences):
     def draw(self, context):
         layout = self.layout
 
+#        row = layout.row()
+#        col = row.column()
+#        col.label(text="Tab Category:")
+#        col.prop(self, "category", text="")
         box = layout.box()
         row = box.row(align=True)
         row.label(text='Panel Category (3D View):')
